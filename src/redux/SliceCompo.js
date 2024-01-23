291159e83ab12d9a3aeb9897ab7b665a405c3aa5
meshv <meshvpatel.ics@gmail.com>
2024-01-23 16:41:28 +0530

    delete bug solved

--- src/redux/SliceCompo.js
+++ src/redux/SliceCompo.js
@@ -1,7 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
-
     const initialState = {
         arr: [],
     }
@@ -16,7 +14,7 @@ export const SliceCompo = createSlice({
         },
         deleteValue : (state, {payload}) =>{
             console.log(payload)
-            state.arr.splice(payload, payload+1)
+            state.arr.splice(payload, 1)
         },
         updateValue: (state, {payload})=>{
             state.arr[payload.key].inputtxt = payload.inputtxt;
